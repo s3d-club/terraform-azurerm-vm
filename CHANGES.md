bcceac8799f9b0495961cfbba35bbc5c3b29486c
Mark Farnsworth <mark.farnsworth@iqvia.com>
2022-11-18 02:03:26 +0000

    0.0.18 Updated Terraform version
    - Updated `azurerm`
    - Updated Terraform version

--- CHANGES.md
+++ CHANGES.md
@@ -10,17 +10,18 @@ This file describes the project history as it relates to tagged versions.
 - Updated `.terraform.lock.hcl`
 - Updated `name` module
 
-## [0.0.15](.)
+## [0.0.15](.) to [0.0.16](.)
 - Sorted `variables.tf`
 - Updated `name` module
 - Updated `terraform` version
 - Updated `time` provider
-
-## [0.0.16](.)
-- Updated version
 
 ## [0.0.17](.)
 - Updated `azurerm` provider
 - Updated `external` provider
 - Updated `name` module
 - Updated `time` provider
+
+## [0.0.18](.)
+- Updated `azurerm`
+- Updated Terraform version
--- terraform.tf
+++ terraform.tf
@@ -1,25 +1,10 @@
 terraform {
-  required_version = ">=1.3.4"
+  required_version = ">=1.3.5"
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.31.0"
-    }
-
-    external = {
-      source  = "hashicorp/external"
-      version = ">=2.2.3"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">=3.4.3"
-    }
-
-    time = {
-      source  = "hashicorp/time"
-      version = ">=0.9.1"
-    }
+    azurerm  = ">=3.32.0"
+    external = ">=2.2.3"
+    random   = ">=3.4.3"
+    time     = ">=0.9.1"
   }
 }
